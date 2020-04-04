f949dbe4deb5dfe5f946101ba5da444f063f8d80
Raul Glogovetan <raul@glogovetan.com>
2020-04-04 16:15:05 +0300

    Initial setup of markdown support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby`,
+    description: `Powered by Gatsby`,
+    // author: `@rdig`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,6 +25,21 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
