0b756688a8f9f36a9bd7eb90ec50c7a5c6f691ee
Raul Glogovetan <raul@glogovetan.com>
2020-05-06 21:54:05 +0300

    Refactor: `Footer` to include some metadata

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,10 @@
 import React, { FC } from 'react';
 
+import ExternalLink from '@components/ExternalLink';
+
+import { version, repository } from '../../../package.json';
+import { LINKS } from '@constants';
+
 import styles from './Footer.module.css';
 
 interface Props {
@@ -12,9 +17,37 @@ const defaultProps: DefaultProps = {
   title: '',
 };
 
+const Gatsby = () => (
+  <>
+    &nbsp;Built on <ExternalLink to={LINKS.GATSBY}>Gatsby</ExternalLink>.
+  </>
+);
+
+const Github = () => (
+  <>
+    &nbsp;Hosted on <ExternalLink to={LINKS.GH_PAGES}>
+      Github Pages
+    </ExternalLink>.
+  </>
+);
+
+const ReleaseNotes = () => (
+  <>
+    &nbsp;<ExternalLink to={`${repository?.url}/releases/${version}`}>
+      Release Notes
+    </ExternalLink>
+  </>
+);
+
 const Footer = ({ title }: Props) => (
   <footer className={styles.main}>
     <p>Copyright {title} © {new Date().getFullYear()}</p>
+    <p>
+      Version {version}.
+      <Gatsby />
+      <Github />
+      <ReleaseNotes />
+    </p>
   </footer>
 );
 
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,8 @@ export const ROUTES = {
   ABOUT: '/about',
   RESUME: '/resume',
 };
+
+export const LINKS = {
+  GH_PAGES: 'https://pages.github.com/',
+  GATSBY: 'https://www.gatsbyjs.org/',
+};
